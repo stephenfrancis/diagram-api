f88038262a63925f3089ea9e5ad779f6b9877a95
Stephen Francis <stephen.francis@helloself.com>
2022-02-20 20:11:58 +0000

    update react-router and deploy to firebase

--- src/examples/index.tsx
+++ src/examples/index.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Route, Switch } from "react-router";
+import { Route, Routes } from "react-router";
 import { BrowserRouter, Link } from "react-router-dom";
 import Basic from "./basic/app";
 import CornerStitch from "./corner-stitch/app";
 import LGOP from "./lgop/app/App";
+
+const Menu: React.FC<{}> = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/basic">Basic</Link>
+      </li>
+      <li>
+        <Link to="/corner-stitch">Corner Stitch</Link>
+      </li>
+      <li>
+        <Link to="/lgop">LGOP</Link>
+      </li>
+    </ul>
+  );
+};
 
 const App: React.FC<{}> = () => {
   return (
@@ -12,30 +28,12 @@ const App: React.FC<{}> = () => {
       <nav>
         <Link to="/">Home</Link>
       </nav>
-      <Switch>
-        <Route path="/basic">
-          <Basic />
-        </Route>
-        <Route path="/corner-stitch">
-          <CornerStitch />
-        </Route>
-        <Route path="/lgop">
-          <LGOP />
-        </Route>
-        <Route>
-          <ul>
-            <li>
-              <Link to="/basic">Basic</Link>
-            </li>
-            <li>
-              <Link to="/corner-stitch">Corner Stitch</Link>
-            </li>
-            <li>
-              <Link to="/lgop">LGOP</Link>
-            </li>
-          </ul>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/basic" element={<Basic />} />
+        <Route path="/corner-stitch" element={<CornerStitch />} />
+        <Route path="/lgop" element={<LGOP />} />
+        <Route path="*" element={<Menu />} />
+      </Routes>
     </BrowserRouter>
   );
 };
